2eb3992ef2f40fda8fd718e61e179d31ca4bc06a
Shantanu Sharma <sshantanu914@gmail.com>
2026-02-20 09:18:28 +0000

    security-ui

---
+++ security-ui/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("securityui.controller.View1", {
+        onInit() {
+        }
+    });
+});
---
+++ security-ui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["securityui/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
